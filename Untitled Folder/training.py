cc7b3f3c3df8bc4cf3f3233ad5e6aaeff807ae25
Joar Gruneau <joar@gruneau.se>
2018-04-27 15:25:30 +0200

    final draft up to weight functions

--- Untitled Folder/training.py
+++ Untitled Folder/training.py
@@ -1,5 +1,5 @@
 for epoch in epochs:
   update_generative_network
   if epoch % check_discriminative_network == 0:
-    while discriminative_loss > cut_off:
+    while discriminative_loss > cutoff:
       update_discriminative_network
